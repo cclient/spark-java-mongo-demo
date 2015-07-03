bc5dac0a872f9e237f1e47f96f7bfa1848fe46c2
ccddpp <cccdddppp@163.com>
2015-07-03 15:34:20 +0800

    first

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # spark-java-mongo-demo
-java实现spark分析mongodb数据，还未完成
+spark
+通过 mongo-hadoop
+分析 mongodb数据
+java写成
+
+
+执行方式
+root@debian01:/usr/local/spark-1.3.1-bin-hadoop2.6# bin/spark-submit --class "sparkfisrttest.cdpspark.App" --packages org.mongodb.mongo-hadoop:mongo-hadoop-core:1.3.1,org.mongodb:mongodb-driver:3.0.1,org.mongodb:mongo-java-driver:3.0.1 ~/hadoop-spark-mongo-examples.jar 
+
+个人测试通过
+
--- src/main/java/sparkfisrttest/cdpspark/App.java
+++ src/main/java/sparkfisrttest/cdpspark/App.java
@@ -1,7 +1,5 @@
 package sparkfisrttest.cdpspark;
 
-
-
 import java.util.Iterator;
 import java.util.List;
 
@@ -23,16 +21,13 @@ import com.mongodb.hadoop.util.MapredMongoConfigUtil;
 import com.mongodb.hadoop.util.MongoConfigUtil;
 //import com.mongodb.hadoop.util.MongoTool;
 
+
+
+
+
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
-
-
-
-
-
-
-
-
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.util.ToolRunner;
@@ -46,43 +41,59 @@ import org.apache.hadoop.mapreduce.Job;
 //import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.bson.BSONObject;
 
-
+import scala.Tuple2;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-	public App(){
-		
+public class App {
+	public App() {
 	}
-    public static void main( String[] args )
+	public static void main( String[] args )
     {
     	SparkConf conf = new SparkConf().setAppName("mongo apark").setMaster("local");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		Configuration hadoopconf = new Configuration();
+		
+		
 		MongoConfig config = new MongoConfig(hadoopconf);
+		
 		config.setInputURI("mongodb://172.16.231.1:27017/lewifi.auditOrigData");
-		config.setOutputURI("mongodb://172.16.231.1:27017/lewifi.monthtopurlwithcombine");
+		config.setQuery("{apMac:{$in:[\"00:21:26:00:10:A3\",\"00:21:26:00:14:C3\"]}}");
+		config.setOutputURI("mongodb://172.16.231.1:27017/lewifi.monthtopurlwithcombine2");
+		
+		config.setOutputKey(Text.class);
+		config.setOutputValue(BSONWritable.class);
+		
 		config.setInputFormat(MongoInputFormat.class);
 		config.setOutputFormat(MongoOutputFormat.class);		
-		JavaPairRDD  rdd=sc.newAPIHadoopRDD(hadoopconf, MongoInputFormat.class, Object.class, BSONObject.class);
-//		rdd.map(new Function<Object, BSONObject>() {
-//		      public BSONObject call(BSONObject integer) {
-//		        double x = Math.random() * 2 - 1;
-//		        double y = Math.random() * 2 - 1;
-//		        return (x * x + y * y < 1) ? 1 : 0;
-//		      }
-//		    });
-		JavaRDD  jdd=	rdd.values();
-		List ls=	jdd.collect();
-		Iterator i = ls.iterator(); 
-		while(i.hasNext()){
-			  com.mongodb.BasicDBObject obj=(BasicDBObject) i.next();
-			  String str= obj.toString();
-			System.out.println(obj.getClass());
-			System.out.println(str);	
-		}
+		JavaPairRDD<Object,BSONObject>  rdd=sc.newAPIHadoopRDD(hadoopconf, MongoInputFormat.class, Object.class, BSONObject.class);
+	    JavaPairRDD<Object, BSONObject> resultRDD = rdd.mapToPair(
+	      new PairFunction<Tuple2<Object, BSONObject>, String, String>() {
+	        public Tuple2<String, String> call(Tuple2<Object, BSONObject> e) {
+//	        	 System.out.print(e.toString());
+	          String apmac=(String) e._2().get("apMac");
+	          System.out.print(apmac);
+	          String clientmac=(String) e._2().get("clientMac");
+	          return new Tuple2<String, String>(apmac,clientmac);
+	        }
+	    }).groupByKey().mapToPair(new PairFunction<Tuple2<String,Iterable<String>>,Object,BSONObject>(){
+			public Tuple2<Object, BSONObject> call(
+					Tuple2<String, Iterable<String>> t) throws Exception {
+				BSONObject bson=new BasicDBObject();
+				BSONObject subbson=new BasicDBObject();
+				Iterator<String> iter=t._2.iterator();
+				while(iter.hasNext()){
+					subbson.put( (String) iter.next(), "");
+				}
+				bson.put("apmac", t._1);
+				bson.put("clientmac", subbson);
+				return new Tuple2<Object, BSONObject>(t._1,bson);
+			}
+	    	
+	    });
+	    //第一个参数 没用实际用途，但必须为可用路径
+	    resultRDD.saveAsNewAPIHadoopFile("hdfs:///sparktestdata", Object.class,Object.class,MongoOutputFormat.class, hadoopconf);
     }
 }
